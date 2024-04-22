01c695c1f55c0199964fe4fa88d70f1467b253b0
TharushiBaddegama <baddegamatharushi@gmail.com>
2024-04-23 02:02:53 +0530

    create request

--- Chapter-14/app/routes.py
+++ Chapter-14/app/routes.py
@@ -9,7 +9,8 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
     EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
-
+from langdetect import detect, LangDetectException
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -25,7 +26,12 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -206,3 +212,13 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+    
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    data = request.get_json()
+    return {'text': translate(data['text'],
+                              data['source_language'],
+                              data['dest_language'])}
