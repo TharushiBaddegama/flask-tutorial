3ae2244170537a38ce6a45aaaf8885d14926d8ec
TharushiBaddegama <baddegamatharushi@gmail.com>
2024-04-22 21:10:40 +0530

    create  I18n and L10n

--- Chapter-13/microblog.py
+++ Chapter-13/microblog.py
@@ -2,6 +2,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Post
+from app import cli
 
 @app.shell_context_processor
 def make_shell_context():
